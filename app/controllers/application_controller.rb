7d1da33f50b0d3f5244c0c74af91a501ab4310d6
Todd Schwartzfarb <todd@wirestreak.com>
2015-11-20 00:14:11 -0500

    extended logged in tutor functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_tutor
+
+  def current_tutor
+    @current_tutor ||= Tutor.find_by(id: session[:tutor_id])
+  end
+  
+
 end
